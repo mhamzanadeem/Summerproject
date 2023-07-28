23d4c8414b98fff89f887cf06a9d769b40b57666
Syed Muhammad Hassan Naqvi <114604234+hassan-56@users.noreply.github.com>
2023-07-28 19:02:05 +0500

    Create source.cpp

---
+++ source.cpp
@@ -0,0 +1,12 @@
+#include "User.h"
+#include <iostream>
+using namespace std;
+#include <string>
+
+
+int main()
+{
+	User U1("Hassan","naqvihassan205@gmail.com","22I-0797","i220797@nu.edu.pk", "fast");
+	cout << U1.getEmail();
+	return 0;
+}
