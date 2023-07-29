08d119b73032f1d22f302eaa06feabc0fb17ee06
mhamzanadeem <nadeemmhamza@gmail.com>
2023-07-29 10:39:15 +0500

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,12 +1,1 @@
-#include "User.h"
-#include <iostream>
-using namespace std;
-#include <string>
-
-
-int main()
-{
-	User U1("Hassan","naqvihassan205@gmail.com","22I-0797","i220797@nu.edu.pk", "fast");
-	cout << U1.getEmail();
-	return 0;
-}
+/* this file has been deleted */
