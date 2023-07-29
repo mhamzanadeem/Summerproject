2fa1bdee59727880962284589cbe048663139d16
mhamzanadeem <nadeemmhamza@gmail.com>
2023-07-29 10:37:05 +0500

    Update User.h

--- User.h
+++ User.h
@@ -2,10 +2,14 @@
 #ifndef USER_H
 #define USER_H
 #include <iostream>
-using namespace std;
+#include <fstream>
 #include <string>
+#include <conio.h>
+using namespace std;
+
 class User
 {
+
 	string name;
 	string email;
 	string RollNumber;
@@ -13,19 +17,32 @@ class User
 	string password;
 public:
 	User(string n = "", string m = "", string rol = "", string id = "", string pass = ""); // User Defined Default Constructor
-////Getters
+	// Getters
 	string getName() const;
 	string getEmail() const;
 	string getRollNumber() const;
 	string getLoginID() const;
 	string getPassword() const;
-////Setters
-	void setName(const string& n);
-	void setEmail(const string& m);
-	void setRollNumber(const string& rol);
-	void setLoginID(const string& id);
-	void setPassword(const string& pass);
-};
+	// Setters
+	void setName(const string &n);
+	void setEmail(const string &m);
+	void setRollNumber(const string &rol);
+	void setLoginID(const string &id);
+	void setPassword(const string &pass);
+	// Function to save user data to a text file
+	void saveUserDataToFile(const string &filename) const;
 
+	// Function to read user data from a text file
+	static User readUserDataFromFile(const string &filename);
+
+	// Function to display user data on the terminal
+	void displayUserData() const;
+
+	//Function for signup
+	void signup();
+
+	//Function for signin
+	void signin();
+};
 
 #endif // !USER_H
