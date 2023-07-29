8355ee373f400d03696a9367ff7766f59d87cec9
mhamzanadeem <nadeemmhamza@gmail.com>
2023-07-29 21:09:46 +0500

    Update User.h

--- User.h
+++ User.h
@@ -4,81 +4,98 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 #include <conio.h>
 using namespace std;
 
 class User
 {
-
+public:
 	string name;
 	string email;
-	string RollNumber;
 	string loginID;
 	string password;
-public:
-	User(string n = "", string m = "", string rol = "", string id = "", string pass = ""); // User Defined Default Constructor
+
+	User(string n = "", string m = "", string id = "", string pass = ""); // User Defined Default Constructor
 	// Getters
 	string getName() const;
 	string getEmail() const;
-	string getRollNumber() const;
 	string getLoginID() const;
 	string getPassword() const;
 	// Setters
 	void setName(const string &n);
 	void setEmail(const string &m);
-	void setRollNumber(const string &rol);
 	void setLoginID(const string &id);
 	void setPassword(const string &pass);
-	// Function to save user data to a text file
-	void saveUserDataToFile(const string &filename) const;
-
-	// Function to read user data from a text file
-	static User readUserDataFromFile(const string &filename);
-
+	
 	// Function to display user data on the terminal
-	void displayUserData() const;
+	virtual void displayUserData() const = 0;
 
-	//Function for signup
-	void signup();
+	// Function for signup
+	virtual void signup() = 0;
 
-	//Function for signin
-	void signin();
+	// Function for signin
+	virtual void signin() = 0;
+
+	virtual ~User() = default;
 };
 ///////////////////////////--STUDENT--///////////////////////////////////
 
-class Student :public User
+class Student : public User
 {
+public:
 	string RollNumber;
-	string* enrolledClasses;
+	string *enrolledClasses;
 	int noEnrolledClasses;
 	int maxEnrolledClasses;
-public:
-	Student(string n = "", string m = "", string id = "", string pass = "",string num = "", string rol = "");
-	Student(const Student& other);
-	void setRollNumber(const string& rol);
+
+	Student(string n = "", string m = "", string id = "", string pass = "", string rol = "");
+	Student(const Student &other);
+	void setRollNumber(const string &rol);
 	string getRollNumber() const;
 	~Student();
 	//// Enroll in a class
-	//void newEnrollment(const string& className);
+	void newEnrollment(const string &className);
 
 	//// View enrolled classes
-	//void viewEnrolledClasses() const;
+	void viewEnrolledClasses() const;
 
+	// Operator Overloading
+	// Assignment Operator
+	Student &operator=(const Student &other);
 
-	//Operator Overloading
-	//Assignment Operator
-	Student& operator=(const Student& other);
+	// Function to display user data on the terminal
+	void displayUserData() const override;
 
+	// Function for signup
+	void signup() override;
+
+	// Function for signin
+	void signin() override;
 };
 
-
 ///////////////////////////--TEACHER--///////////////////////////////////
-class Teacher :public User
+class Teacher : public User
 {
-	string subject;
 public:
-	Teacher(string n = "", string m = "", string id = "", string pass = "",string num = "", string sub = "");
+	string subject;
 
+	Teacher(string n = "", string m = "", string id = "", string pass = "", string sub = "");
+
+	void setSubject(const string &sub);
+	string getSubject() const;
+
+	
+	// Function to display user data on the terminal
+	void displayUserData() const override;
+
+	// Function for signup
+	void signup() override;
+
+	// Function for signin
+	void signin() ;
+
+	Teacher readTeacherDataFromFile(const std::string &filename);
 };
 
 #endif // !USER_H
