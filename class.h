94995cef3a88ea3ab7207ff35b73f52129be1f88
Syed Muhammad Hassan Naqvi <114604234+hassan-56@users.noreply.github.com>
2023-07-31 01:46:40 +0500

    Update class.h

--- class.h
+++ class.h
@@ -1,27 +1,32 @@
+#pragma once
 #include <iostream>
 #include <string>
-#include "user.h"
+#include "User.h"
 using namespace std;
 
 class ClassRoom
 {
 	string className;
-	Teacher teacher;
-	Student* enrolledStudents;
-	int noOfStudents;
-	int maxStudents;
+	Teacher* teacher;
+	//Student* enrolledStudents;
+	//int noOfStudents;
+	//int noOfTeachers;
+	//int maxTeachers;
+	//int maxStudents;
 public:
 	ClassRoom(string n, Teacher& teach);
 	ClassRoom(const ClassRoom& other); // Copy constructor
 	~ClassRoom();
 
-	void setTeacher(const Teacher& t); //teacher
-	void enrollStudent(const Student& student); //Enrollment
-	void viewEnrolledStudents() const; //View Students
+	void setTeacher(Teacher& t); //teacher
+	//void enrollStudent(const Student& student); //Enrollment
+	//void viewEnrolledStudents() const; //View Students
 
+	Teacher& getTeacher() const;
+
+	void setValues();
 	//Operator Overloading
 	ClassRoom& operator=(const ClassRoom& other);
 	friend ostream& operator<<(ostream& os, const ClassRoom& classRoom);
 	//friend bool operator==(const ClassRoom& lhs, const ClassRoom& rhs);
 };
-
